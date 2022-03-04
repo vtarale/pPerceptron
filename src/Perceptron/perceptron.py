bee02401a1d78be901178b7383d1926cb60ec2ab
Vihaan Tarale <taralevihaan@gmail.com>
2022-03-04 22:47:31 +0530

    done

--- src/Perceptron/perceptron.py
+++ src/Perceptron/perceptron.py
@@ -2,10 +2,11 @@ import Vectors.vectors as vector
 
 class Perceptron:
     # initlizing weigths
-    def __init__(self, weigths: list, learning_rate):
+    def __init__(self, weigths: list, learning_rate, bias):
         self.w = vector.Vector(weigths)
         self.learning_rate = learning_rate
         self.correct = 0
+        self.bias = bias
     # relu activtion function
     def relu(self, x):
         if x > 0:
@@ -25,19 +26,15 @@ class Perceptron:
     def predict(self, x: list, relu=False, sigmoid=False):
         inputs = vector.Vector(x)
         answer = self.w.dot(inputs)
+        answer = answer + self.bias
         if relu:
             return self.relu(answer)
         if sigmoid:
             return self.sigmoid(answer)
         return self.deflaut_activation(answer)
     # gradient descent
-    def gradient_descent(self, correct, weigth, guess, input):
-        error = correct - guess
-        if error == 0:
-            self.correct = self.correct + 1
-            return weigth
-        new_weigth = weigth + error * input * self.learning_rate
-        return new_weigth
+    def gradient_descent(self, error, x):
+        return error * x * self.learning_rate
     # train the perceptron
     def train(self, inputs, answers, epochs, relu=False, sigmoid=False):
         for epoch in range(epochs):
@@ -45,17 +42,18 @@ class Perceptron:
             i = 0
             for xs in inputs:
                 guess = self.predict(inputs[i], relu=relu, sigmoid=sigmoid)
+                error = answers[i] - guess
+                if error == 0:
+                    self.correct = self.correct + 1
                 for index in range(len(xs)):
-                    self.w.inputs[index] = self.gradient_descent(answers[i], self.w.inputs[index], guess, xs[index])
+                    self.w.inputs[index] = self.w.inputs[index] + self.gradient_descent(error, xs[index])
                 i = i + 1
-            print(f"Epochs:{epoch} Percentage: {(self.correct/len(answers)) * 10}, Correct:{self.correct}")
+            print(f"Epochs:{epoch}, Correct:{self.correct}")
     # test the perceptron
     def test(self, inputs, answers, relu=False, sigmoid=False):
         self.correct = 0
-        i = 0
-        for _ in inputs:
-            guess = self.predict(inputs[i], relu=relu, sigmoid=sigmoid)
-            if guess == answers[i]:
+        for index in range(len(inputs)):
+            guess = self.predict(inputs[index], relu=relu, sigmoid=sigmoid)
+            if guess == answers[index]:
                 self.correct = self.correct + 1
-            i = i + 1
         print(f"Correct: {self.correct}")
--- src/test.py
+++ src/test.py
@@ -6,29 +6,29 @@ class Point:
         self.x, self.y, self.lab = x, y, lab
 
 class Test:
-    def __init__(self, learning_rate):
+    def __init__(self, learning_rate, bias):
         self.points = []
         self.train = []
         self.test = []
         self.answers_train = []
         self.answers_test = []
-        self.brain = p.Perceptron([0.5, 0.1, 0.1], learning_rate)
+        self.brain = p.Perceptron([5, 2], learning_rate, bias)
 
     def create_normal(self, points: int, rangex, rangey):
         for _ in range(points):
             x = random.randint(rangex * -1, rangex)
             y = random.randint(rangey * -1, rangey)
             label = 1
-            if y < 0:
+            if x > y:
                 label = -1
             self.points.append(Point(x, y, label))
     # split training and testing data
     def split(self, r: int):
         for index in range(r):
-            self.train.append([1, self.points[index].x, self.points[index].y])
+            self.train.append([self.points[index].x, self.points[index].y])
             self.answers_train.append(self.points[index].lab)
         for index in range(len(self.points) - r):
-            self.test.append([1, self.points[index].x, self.points[index].y])
+            self.test.append([self.points[index].x, self.points[index].y])
             self.answers_test.append(self.points[index].lab)
     # train the perceptron
     def t(self, epochs, relu=False, sigmoid=False):
@@ -38,8 +38,8 @@ class Test:
         self.brain.test(self.test, self.answers_test, relu=relu, sigmoid=sigmoid)
 
 if __name__ == "__main__":
-    perceptron = Test(10)
-    perceptron.create_normal(300, 500000, 5000000)
-    perceptron.split(290)
-    perceptron.t(5, relu=True)
-    perceptron.te(relu=True)
+    perceptron = Test(1, 1)
+    perceptron.create_normal(30000, 500000, 5000000)
+    perceptron.split(29900)
+    perceptron.t(100)
+    perceptron.te()

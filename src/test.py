edcd42bff1a97e8eb14dc2451cabb83b3937b47f
Vihaan Tarale <66897674+vtarale@users.noreply.github.com>
2025-01-29 21:38:42 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -12,7 +12,7 @@ class Test:
         self.test = []
         self.answers_train = []
         self.answers_test = []
-        self.brain = p.Perceptron([5, 2], learning_rate, bias)
+        self.brain = p.Perceptron([-1, 2], learning_rate, bias)
 
     def create_normal(self, points: int, rangex, rangey):
         for _ in range(points):
@@ -24,22 +24,28 @@ class Test:
             self.points.append(Point(x, y, label))
     # split training and testing data
     def split(self, r: int):
+        self.x = []
+        self.y = []
+        self.label = []
         for index in range(r):
             self.train.append([self.points[index].x, self.points[index].y])
             self.answers_train.append(self.points[index].lab)
         for index in range(len(self.points) - r):
+            self.x.append(self.points[index].x)
+            self.y.append(self.points[index].y)
+            self.label.append(self.points[index].lab)
             self.test.append([self.points[index].x, self.points[index].y])
             self.answers_test.append(self.points[index].lab)
     # train the perceptron
-    def t(self, epochs, relu=False, sigmoid=False):
-        self.brain.train(self.train, self.answers_train, epochs, relu=relu, sigmoid=sigmoid)
+    def t(self, epochs, relu=False):
+        self.brain.train(self.train, self.answers_train, epochs, relu=relu)
     # test the perceptron
-    def te(self, relu=False, sigmoid=False):
-        self.brain.test(self.test, self.answers_test, relu=relu, sigmoid=sigmoid)
+    def te(self, relu=False):
+        self.brain.test(self.test, self.answers_test, self.x, self.y, self.label, relu=relu)
 
 if __name__ == "__main__":
-    perceptron = Test(1, 1)
-    perceptron.create_normal(3000, 500000, 5000000)
-    perceptron.split(2900)
-    perceptron.t(10)
-    perceptron.te()
+    perceptron = Test(2.5, 2)
+    perceptron.create_normal(1000, 500, 500)
+    perceptron.split(750)
+    perceptron.t(0, relu=True)
+    perceptron.te(relu=True)   
